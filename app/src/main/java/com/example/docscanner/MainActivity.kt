dd02ee2f4faa326222b62b0d41f487e7a47e2fb1
ballmechai-ux <ballmechai@gmail.com>
2026-05-08 15:24:10 +0700

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/docscanner/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.example.docscanner
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
