853ae993f51e029c40cd5c4e5b14c9f630a6d8b8
ballmechai-ux <ballmechai@gmail.com>
2026-05-09 15:55:12 +0700

    Delete app/src/main directory

--- app/src/main/java/com/example/docscanner/MainActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.example.docscanner
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-}
