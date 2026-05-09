b80fac25196957225c4a980313d7a6cf616f81ac
GitHub Action <action@github.com>
2026-05-09 14:05:15 +0000

    Fix all project files

---
+++ app/src/main/java/com/example/docscanner/PreviewActivity.kt
@@ -0,0 +1,8 @@
+package com.example.docscanner
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+class PreviewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
